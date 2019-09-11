5f28bbd7d333b6e5ebdb83c2490eb4e0e554b02b
edward01s2 <edward.01s2@gmail.com>
2019-09-11 13:31:46 -0500

    Fix form logic

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,59 +9,105 @@ const inputField = css`
   ${tw`appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`};
 `
 
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
 class ContactPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      isValidated: false,
+      isSubmitted: false, 
+    }
+    this.baseState = this.state;
+  }
+
+  resetForm = () => {
+    this.setState(this.baseState)
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => {
+        this.setState({isSubmitted: true})
+      })
+      //navigate(form.getAttribute('action'))
+      .catch(error => alert(error))
+  }
+
   render() {
     const data = this.props.data.markdownRemark
 
     return (
       <Layout>
         <div dangerouslySetInnerHTML={{ __html: data.html }} />
+        <div className={`mt-12 ${this.state.isSubmitted ? '' : 'hidden'}`}>
+          <p className="text-xl font-bold">Your message has been sent. Thank you for getting in touch! </p>
+        </div>
         <form
           name="contact"
-          className="mt-16"
+          className={`mt-12 ${this.state.isSubmitted ? 'hidden' : ''}`}
+          action="/contact/thanks/"
           netlify-honeypot="bot-field"
           method="POST"
           data-netlify="true"
+          onSubmit={this.handleSubmit}
         >
-          <p class="hidden">
+          <p className="hidden">
             <label>
-              Don’t fill this out if you're human: <input name="bot-field" />
+              Don’t fill this out if you're human: <input type="hidden" name="bot-field" onChange={this.handleChange} />
             </label>
           </p>
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-name"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-name"
               >
                 Name
               </label>
-              <input css={inputField} id="grid-name" type="text" required />
+              <input css={inputField} id="grid-name" name="name" type="text" onChange={this.handleChange} required />
             </div>
-            <div class="w-full md:w-1/2 px-3">
+            <div className="w-full md:w-1/2 px-3">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-email"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-email"
               >
                 Email
               </label>
-              <input css={inputField} id="grid-email" type="email" required />
+              <input css={inputField} id="grid-email" type="email" name="email" onChange={this.handleChange} required />
             </div>
           </div>
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-message"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-message"
               >
                 Message
               </label>
-              <textarea css={inputField} id="grid-message" rows="4" required />
+              <textarea css={inputField} id="grid-message" name="message" rows="4" onChange={this.handleChange} required />
             </div>
           </div>
-          <div class="flex justify-end mb-6">
+          <div className="flex justify-end mb-6">
             <button
-              class="bg-gray-600 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-gray-600 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
               Submit
